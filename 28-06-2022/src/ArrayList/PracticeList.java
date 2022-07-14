e31b52ab1f39248fdee5f6d0e953af4a0d7db6e1
vamshikrishna110 <106948168+vamshikrishna110@users.noreply.github.com>
2022-07-14 17:36:02 +0530

    Add files via upload

---
+++ 28-06-2022/src/ArrayList/PracticeList.java
@@ -0,0 +1,16 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class PracticeList {
+
+	public static void main(String[] args) {
+		ArrayList li = new ArrayList();
+		li.add("hello");
+		
+		System.out.println(li);
+
+
+	}
+
+}
